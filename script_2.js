bcb1f361a167ad2a730778db9a1d668c904fc48a
CSaditya7 <137849324+CSaditya7@users.noreply.github.com>
2025-07-22 19:06:42 +0530

    Add files via upload

---
+++ script_2.js
@@ -0,0 +1,13 @@
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/687f8db360e51e191fc553ef/1j0p432dc';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
